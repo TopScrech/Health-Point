14b7d8a05caaa367290a2ea38b6e6db374526b28
TopScrech <sergei_saliukov@icloud.com>
2025-11-21 14:34:15 +0100

    new data types

--- iOS/Dashboard/Dashboard.swift
+++ iOS/Dashboard/Dashboard.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 
 struct Dashboard: View {
-    private let gridItems: [GridItem] = [
+    private let columns = [
         GridItem(.flexible(minimum: 100, maximum: 200)),
         GridItem(.flexible(minimum: 100, maximum: 200))
     ]
     
     var body: some View {
-        LazyVGrid(columns: gridItems) {
+        LazyVGrid(columns: columns) {
             DashboardCard(title: "Steps", icon: "figure.walk", iconColor: .yellow, value: "911")
             DashboardCard(title: "Heart Rate", icon: "heart", iconColor: .red, value: "80", unit: "bpm")
             DashboardCard(title: "Oxygen", icon: "drop", iconColor: .blue, value: "98", unit: "%")
@@ -17,6 +17,9 @@ struct Dashboard: View {
             DashboardCard(title: "Carbs", icon: "carrot", iconColor: .green, value: "120", unit: "g")
             DashboardCard(title: "Insulin", icon: "syringe", iconColor: .blue, value: "16", unit: "units")
             DashboardCard(title: "Calories", icon: "flame", iconColor: .orange, value: "2000", unit: "kcal")
+            DashboardCard(title: "Blood Glucose", icon: "waveform.path.ecg.rectangle", iconColor: .red, value: "16.16", unit: "mmol/L")
+            DashboardCard(title: "Handwashing", icon: "hands.and.sparkles", iconColor: .blue, value: "20", unit: "sec (avg)")
+            DashboardCard(title: "Sleep", icon: "bed.double", iconColor: .purple, value: "4:58")
         }
     }
 }
